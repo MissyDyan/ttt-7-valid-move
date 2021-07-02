2453ea2b69033b1a4dfa86b88c5cd644d4558f99
Diane Nguyen <diane.nguyen@gmail.com>
2021-07-01 22:39:00 -0700

    Valid Tic Tac Toe Move assignment completed.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,31 @@
 # code your #valid_move? method here
-
+def valid_move?(board, player_input)
+    if board[player_input] == " " || board[player_input] == "" 
+        return true
+    else  player_input == nil
+        return false
+    end
+end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+
+# previous assignment:
+# def position_taken?(board, player_input)
+#     if board[player_input] == " " 
+#         return false
+#     elsif board[player_input] == "" || board[player_input] == nil
+#         return false
+#     else board[player_input] == "X" || board[player_input] == "O"
+#         return true
+#     end
+# end
+
+def valid_move?(board, player_input) 
+    if board[player_input] == " " || board[player_input] == "" 
+        return true
+    elsif board[player_input] == nil
+        return false
+    else board[player_input] == "X" || board[player_input] == "O"
+        return false
+    end
+end
